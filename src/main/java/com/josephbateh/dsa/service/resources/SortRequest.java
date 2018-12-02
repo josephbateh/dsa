331da7a497f761416d71f89df37c75b71fae02b6
Joseph Bateh <josephbateh@gmail.com>
2018-12-01 19:22:57 -0500

    Lombok
    - Add Lombok
    - Setup de-serialization correctly

--- src/main/java/com/josephbateh/dsa/service/resources/SortRequest.java
+++ src/main/java/com/josephbateh/dsa/service/resources/SortRequest.java
@@ -1,7 +1,14 @@
 package com.josephbateh.dsa.service.resources;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class SortRequest {
-  private List<Integer> list;
+  public List<Integer> list;
 }
