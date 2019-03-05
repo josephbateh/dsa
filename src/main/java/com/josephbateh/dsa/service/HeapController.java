2d07b0961134fd927fd119b3818819bcb8bad908
Joseph Bateh <josephbateh@gmail.com>
2019-03-04 22:36:58 -0500

    Heaps and Quick Sort
    - Add shell code for min/max heaps
    - Add shell code for quick sort
    - Add NotImplemented endpoints for heaps
    - Add NotImplemented endpoint for quick sort
    - Add unit test shell for heaps
    - Add unit test shell for quick sort
    - Add API test shell for heaps
    - Add API test shell for quick sort

--- src/main/java/com/josephbateh/dsa/service/HeapController.java
+++ src/main/java/com/josephbateh/dsa/service/HeapController.java
@@ -12,12 +12,19 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping(value = "/heap", produces = MediaType.APPLICATION_JSON_VALUE)
 public class HeapController {
-    protected static Logger log = LoggerFactory.getLogger(HeapController.class);
+  protected static Logger log = LoggerFactory.getLogger(HeapController.class);
 
-    @RequestMapping(method = RequestMethod.POST, value = "/min")
-    @ResponseStatus(value = HttpStatus.OK)
-    public ResponseEntity<HeapResponse> minHeap(@RequestBody HeapRequest request) {
-        log.info("Adding {} integers to a min heap", request.getList().size());
-        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
-    }
+  @RequestMapping(method = RequestMethod.POST, value = "/min")
+  @ResponseStatus(value = HttpStatus.OK)
+  public ResponseEntity<HeapResponse> minHeap(@RequestBody HeapRequest request) {
+    log.info("Adding {} integers to a min heap", request.getList().size());
+    return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+  }
+
+  @RequestMapping(method = RequestMethod.POST, value = "/max")
+  @ResponseStatus(value = HttpStatus.OK)
+  public ResponseEntity<HeapResponse> maxHeap(@RequestBody HeapRequest request) {
+    log.info("Adding {} integers to a max heap", request.getList().size());
+    return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+  }
 }
