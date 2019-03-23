310e5279aada4ea67adeb639e955dee368256e07
Joseph Bateh <github@josephbateh.com>
2019-03-23 14:49:34 -0400

    Min Heap
    - Create and implement MinHeap class
    - Write test cases for MinHeap class
    - Add Min Heap to API
    - Remove Maven Files In Commits

--- src/main/java/com/josephbateh/dsa/service/HeapController.java
+++ src/main/java/com/josephbateh/dsa/service/HeapController.java
@@ -1,5 +1,7 @@
 package com.josephbateh.dsa.service;
 
+import com.josephbateh.dsa.domain.heap.MinHeap;
+import com.josephbateh.dsa.helpers.Timer;
 import com.josephbateh.dsa.service.resources.HeapRequest;
 import com.josephbateh.dsa.service.resources.HeapResponse;
 import org.slf4j.Logger;
@@ -9,6 +11,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/heap", produces = MediaType.APPLICATION_JSON_VALUE)
 public class HeapController {
@@ -16,9 +21,21 @@ public class HeapController {
 
   @RequestMapping(method = RequestMethod.POST, value = "/min")
   @ResponseStatus(value = HttpStatus.OK)
-  public ResponseEntity<HeapResponse> minHeap(@RequestBody HeapRequest request) {
+  public HeapResponse minHeap(@RequestBody HeapRequest request) {
+    Timer timer = new Timer();
+    timer.start();
+
     log.info("Adding {} integers to a min heap", request.getList().size());
-    return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    MinHeap heap = new MinHeap(request.getList().toArray(new Integer[0]));
+    long duration = timer.stop();
+    log.info("Finished adding {} integers to a min heap", request.getList().size());
+
+    List<Integer> list = new ArrayList<>();
+    while (heap.hasElements()) {
+      list.add(heap.pop());
+    }
+
+    return new HeapResponse(duration, list);
   }
 
   @RequestMapping(method = RequestMethod.POST, value = "/max")
